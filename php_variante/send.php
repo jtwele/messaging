139465551f2d05870b16ee9d035bf1d7283d45b4
jeremias.twele <jeremias@twele.net>
2015-05-04 17:45:41 +0200

    test Senden mit Function

--- php_variante/send.php
+++ php_variante/send.php
@@ -1,28 +1,24 @@
 <?php
-
 require_once __DIR__ . '/libs/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+class Messaging {
+	
+	
+	function send($nachricht) {
+		$connection = new AMQPConnection ( '141.22.29.97', 5672, 'invoiceSender', 'invoiceSender' );
+		$channel = $connection->channel ();
+		$channel->queue_declare ( 'controllerInvoice', false, false, false, false );
+		
+		settype ( $nachricht, "string" );
+		$msg = new AMQPMessage ( $nachricht );
+		
+		$channel->basic_publish ( $msg, '', 'controllerInvoice' );
 
-$connection = new AMQPConnection('141.22.29.97', 5672, 'invoiceSender', 'invoiceSender');
-
-$channel = $connection->channel();
-$channel->queue_declare('controllerInvoice', false, false, false, false);
-
-for ($x = 0; $x <= 10; $x++) {
-
-	$idx = $x;   // boolean
-	settype($bar, "string");
-
-    	echo "The number is: $x <br>";
-
-	$msg = new AMQPMessage('nachricht: $bar <br>');
-
-	$channel->basic_publish($msg, '', 'controllerInvoice');
-
-	echo " [x] Sent 'Hello World!'\n";
+		$channel->close ();
+		$connection->close ();
+	}
 }
-$channel->close();
-$connection->close();
+
 ?>
